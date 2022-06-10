8f979266bfad202222e2abf9fe6164103c369d26
NBeshev <beshev.code@gmail.com>
2022-06-10 01:16:11 +0100

    mongooseCLI

--- src/movie/model.js
+++ src/movie/model.js
@@ -1,21 +1,22 @@
 const mongoose = require("mongoose");
 
-//Creating a schima which should be called what the model will be called + Schema (modelSchema)
+//Creating a schema which should be called what the model will be called + Schema (modelSchema)
 //Schema is class construction
 const movieSchema = new mongoose.Schema({
-  //Defining key value pare "title"
+  //Defining key value pair "title"
   title: {
     type: String, //It wouldn't work if the title is integer for instance
     unique: true, //No more than one of the same movie title can exist in the database
     required: true //It must be provided
   },
-  //Defining key value pare "actor"
+  //Defining key value pair "actor"
   actor: {
     type: String, //It wouldn't work if the title is integer for instance
     default: "Not Specified" //If not provided will be saved in the database as "Not Specified"
   }
 });
 
+//Giving our schema to a model
 const Movie = mongoose.model("Movie", movieSchema);
 
 module.exports = Movie;
